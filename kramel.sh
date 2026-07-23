579b838d25377ae6eaddde6b5bc4d1d1b8051a77
B-SideU <150783809+B-SideU@users.noreply.github.com>
2026-07-23 04:26:26 -0400

    Update kramel.sh

--- kramel.sh
+++ kramel.sh
@@ -529,11 +529,7 @@ mkzip() {
 		tg "*Building zip!*"
 	fi
 	echo -e "\n\e[1;93m[*] Building zip! \e[0m"
-	cat "${DIST_DIR}"/mt6855.dtb >"${AK3}"/dtb || abort "Failed to concatenate mt6855.dtb to AnyKernel3 directory!"
-	cp "${DIST_DIR}"/dtbo.img "${AK3}"/dtbo.img || abort "Failed to copy dtbo.img to AnyKernel3 directory!"
 	cp -p "${DIST_DIR}"/Image.gz "${AK3}"/ || abort "Failed to copy Image.gz to AnyKernel3 directory!"
-	cp -p "${DIST_DIR}"/dlkm.cpio.lz4 "${AK3}"/modules/ || abort "Failed to copy LZ4-compressed CPIO archive for vendor_ramdisk modules to AnyKernel3 directory!"
-	cp -p "${DIST_DIR}"/dlkm.tar.xz "${AK3}"/modules/ || abort "Failed to copy XZ-compressed tarball for vendor_dlkm modules to AnyKernel3 directory!"
 	cd "${AK3}" || abort "Failed to cd into AnyKernel3 directory!"
 	zip -r9 "$zipn".zip . -x ".git*" -x "*.zip" || abort "Failed to package and compress AnyKernel3 directory!"
 	echo -e "\n\e[1;32m[✓] Built zip! \e[0m"
